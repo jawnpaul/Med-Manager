d2078da08917d94b02ff2af339305c166f76c805
John Paul <jpaul5246@gmail.com>
2018-03-28 20:53:22 +0100

    Added navigation drawer, login flow, launcher icon, and customized the toolbar

--- app/src/main/java/ng/org/knowit/med_manager/MainActivity.java
+++ app/src/main/java/ng/org/knowit/med_manager/MainActivity.java
@@ -1,8 +1,20 @@
 package ng.org.knowit.med_manager;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
@@ -14,6 +26,12 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mNavigationView;
+    private Toolbar mToolbar;
+    private FloatingActionButton mFloatingActionButton;
+
     private static final int RC_SIGN_IN = 123;
 
     @Override
@@ -21,10 +39,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Initialize all the views
+        initializeViews();
 
 
+        setSupportActionBar(mToolbar);
+        //noinspection deprecation
+        mToolbar.setTitleTextColor(getResources().getColor(R.color.colorAccent));
 
-// Choose authentication providers
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_action_menu);
+
+
+        mFloatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        mNavigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                        menuItem.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
+
+        // Choose authentication providers
         @SuppressWarnings("deprecation") List<AuthUI.IdpConfig> providers = Arrays.asList(
                 new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
                 new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build());
@@ -36,19 +83,44 @@ public class MainActivity extends AppCompatActivity {
                         .setAvailableProviders(providers)
                         .build(),
                 RC_SIGN_IN);
-
     }
+
+    private void initializeViews(){
+        mToolbar = findViewById(R.id.toolbar);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mNavigationView = findViewById(R.id.nav_view);
+        mFloatingActionButton = findViewById(R.id.fab);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == RC_SIGN_IN) {
-            @SuppressWarnings("UnusedAssignment") IdpResponse response = IdpResponse.fromResultIntent(data);
+            @SuppressWarnings("UnusedAssignment") IdpResponse response =
+                    IdpResponse.fromResultIntent(data);
 
             //noinspection StatementWithEmptyBody
             if (resultCode == RESULT_OK) {
                 // Successfully signed in
-                @SuppressWarnings("UnusedAssignment") FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                @SuppressWarnings("UnusedAssignment") FirebaseUser user =
+                        FirebaseAuth.getInstance().getCurrentUser();
                 // ...
             } else {
                 // Sign in failed, check response for error code
@@ -56,4 +128,5 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
 }
